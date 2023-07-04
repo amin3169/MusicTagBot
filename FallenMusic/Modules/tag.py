91dbe5157754f65a9ee94f2889cfd7a21c2ca701
Aliyev Ali <108521240+offlineflood@users.noreply.github.com>
2023-07-04 18:05:26 +0400

    Update tag.py

--- FallenMusic/Modules/tag.py
+++ FallenMusic/Modules/tag.py
@@ -3,15 +3,7 @@ from telethon import TelegramClient, events
 import random, asyncio, logging
 from telethon import Button
 import config
-
-logging.basicConfig(level=logging.INFO,format='%(name)s - [%(levelname)s] - %(message)s')
-LOGGER = logging.getLogger(__name__)
-
-api_id = config.API_ID
-api_hash = config.API_HASH
-bot_token = config.BOT_TOKEN
-dejavu = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
-
+from FallenMusic import dejavu
 
 anlik_calisan = []
 tekli_calisan = []
