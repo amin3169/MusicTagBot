b58009aae4eaba2269db532465ac69c0437a156c
Aliyev Ali <108521240+offlineflood@users.noreply.github.com>
2023-07-04 16:06:45 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,27 +1,18 @@
 from os import getenv
-
 from dotenv import load_dotenv
 
 load_dotenv()
-
-
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
-
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
-
 OWNER_ID = int(getenv("OWNER_ID"))
-
-PING_IMG = getenv("PING_IMG", "https://graph.org/file/16805c3220aff0f9b74be.jpg")
-START_IMG = getenv("START_IMG", "https://graph.org/file/16805c3220aff0f9b74be.jpg")
-
+PING_IMG = getenv("PING_IMG")
+START_IMG = getenv("START_IMG")
 SESSION = getenv("SESSION", None)
-
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/justtalkinggrop")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/justtalkinggrop")
-
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6212838686").split()))
+SUPPORT_CHAT = getenv("SUPPORT_CHAT")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
 
 
-FAILED = "https://graph.org/file/52cc2b744f86802aa82ac.jpg"
+FAILED = "https://graph.org/file/35444b7d40fff28719e62.jpg"
