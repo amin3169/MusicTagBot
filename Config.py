2211b0b7c58238538a0541d3eac8caa132375fec
RaviVeyi <112951109+RaviVeyi@users.noreply.github.com>
2023-07-08 10:52:10 +0400

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,7 +1,27 @@
-import os
+from os import getenv
 
-class Config():
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    OWNER_ID = int(os.environ.get("OWNER_ID"))
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+
+BOT_TOKEN = getenv("BOT_TOKEN", None)
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
+
+OWNER_ID = int(getenv("OWNER_ID"))
+
+PING_IMG = getenv("PING_IMG")
+START_IMG = getenv("START_IMG")
+
+SESSION = getenv("SESSION", None)
+
+SUPPORT_CHAT = getenv("SUPPORT_CHAT")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL")
+
+SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
+
+
+FAILED = ""
