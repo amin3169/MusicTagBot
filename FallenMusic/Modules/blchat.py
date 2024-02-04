962d863448c61f5db472a98d4c352c88c33e1078
VeyisRavi <112951109+VeyisRavi@users.noreply.github.com>
2024-02-04 12:46:55 +0400

    Update blchat.py
    Signed-off-by: VeyisRavi <112951109+VeyisRavi@users.noreply.github.com>

--- FallenMusic/Modules/blchat.py
+++ FallenMusic/Modules/blchat.py
@@ -1,10 +1,10 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import blacklist_chat, blacklisted_chats, whitelist_chat
-from AnonXMusic.utils.decorators.language import language
+from FallenMusic import app
+from FallenMusic.misc import SUDOERS
+from FallenMusic.utils.database import blacklist_chat, blacklisted_chats, whitelist_chat
+from FallenMusic.utils.decorators.language import language
 from config import BANNED_USERS
 
 
